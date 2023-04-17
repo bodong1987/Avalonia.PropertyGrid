745e83a6441a283b0affc039871693e31584479d
bodong <bodong@tencent.com>
2023-04-17 17:51:40 +0800

    add a mstest projects.

---
+++ Avalonia.PropertyGrid.Model/Utils/PropertyDescriptorBuilder.cs
@@ -0,0 +1,161 @@
+﻿using Avalonia.PropertyGrid.Model.ComponentModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia.PropertyGrid.Model.Utils
+{
+    /// <summary>
+    /// Class PropertyDescriptorBuilder.
+    /// </summary>
+    public class PropertyDescriptorBuilder
+    {
+        /// <summary>
+        /// The is multiple objects
+        /// </summary>
+        public readonly bool IsMultipleObjects;
+        /// <summary>
+        /// The target
+        /// </summary>
+        public readonly Object Target;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyDescriptorBuilder"/> class.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <exception cref="System.ArgumentNullException">target</exception>
+        public PropertyDescriptorBuilder(object target)
+        {
+            if (target == null)
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
+
+            var enumerable = target as IEnumerable;
+            if(enumerable != null)
+            {
+                IsMultipleObjects = true;
+                Target = enumerable;
+            }
+            else
+            {
+                IsMultipleObjects = false;
+                Target = target;
+            }
+        }
+
+        /// <summary>
+        /// Gets the properties.
+        /// </summary>
+        /// <returns>PropertyDescriptorCollection.</returns>
+        public PropertyDescriptorCollection GetProperties()
+        {
+            if(IsMultipleObjects)
+            {
+                return GetMultipleProperties(Target as IEnumerable);
+            }
+            else
+            {
+                return GetProperties(Target);
+            }
+        }
+
+        /// <summary>
+        /// Gets the properties.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <returns>PropertyDescriptorCollection.</returns>
+        private PropertyDescriptorCollection GetProperties(object target)
+        {
+            if(target is ICustomTypeDescriptor ctd)
+            {
+                return ctd.GetProperties();
+            }
+
+            return TypeDescriptor.GetProperties(target);
+        }
+
+        /// <summary>
+        /// Gets the multiple properties.
+        /// </summary>
+        /// <param name="targets">The targets.</param>
+        /// <returns>PropertyDescriptorCollection.</returns>
+        private PropertyDescriptorCollection GetMultipleProperties(IEnumerable targets)
+        {
+            List<PropertyDescriptorCollection> Collections = new List<PropertyDescriptorCollection>();
+            foreach(var target in targets)
+            {
+                Collections.Add(GetProperties(target));
+            }
+
+            if(Collections.Count == 0)
+            {
+                return new PropertyDescriptorCollection(null);
+            }
+
+            //
+            List<MultiObjectPropertyDescriptor> Descriptors = new List<MultiObjectPropertyDescriptor>();
+
+            List<PropertyDescriptorCollection> MultiCollections = Collections.Skip(1).ToList();
+
+            foreach (PropertyDescriptor propertyDescriptor in Collections.First())
+            {
+                if (!AllowMerge(propertyDescriptor))
+                {
+                    continue;
+                }
+
+                bool IsMatched = true;
+                List<PropertyDescriptor> _propertyDescriptors = new List<PropertyDescriptor>(Collections.Count) { propertyDescriptor };
+
+                foreach(var collection in MultiCollections)
+                {
+                    var pd = collection.Find(propertyDescriptor.Name, false);
+
+                    if(pd == null || !pd.Equals(propertyDescriptor))
+                    {
+                        IsMatched = false;
+                        break;
+                    }
+
+                    _propertyDescriptors.Add(pd);
+                }
+
+                if(!IsMatched)
+                {
+                    continue;
+                }
+
+                Descriptors.Add(new MultiObjectPropertyDescriptor(_propertyDescriptors.ToArray()));
+            }
+
+            return new PropertyDescriptorCollection(Descriptors.ToArray());
+        }
+
+        /// <summary>
+        /// Allows the merge.
+        /// </summary>
+        /// <param name="descriptor">The descriptor.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        public static bool AllowMerge(PropertyDescriptor descriptor)
+        {
+            if (descriptor == null)
+            {
+                return false;
+            }
+                
+            MergablePropertyAttribute mergablePropertyAttribute = descriptor.Attributes[typeof(MergablePropertyAttribute)] as MergablePropertyAttribute;
+
+            if (mergablePropertyAttribute == null)
+            {
+                return MergablePropertyAttribute.Default.AllowMerge;
+            }
+                
+            return mergablePropertyAttribute.AllowMerge;
+        }
+    }
+}
---
+++ Avalonia.PropertyGrid.Tests/Model/UnitTest_Utils.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.PropertyGrid.Model.Utils;
+using System.Reflection.Emit;
+
+namespace Avalonia.PropertyGrid.Tests.Model
+{
+    [TestClass]
+    public class UnitTest_Utils
+    {
+        [TestMethod]
+        public void Test_PropertyDescriptorBuilder()
+        {
+            PropertyDescriptorBuilder builder1 = new PropertyDescriptorBuilder(new TestObject());
+
+            var properties =  builder1.GetProperties();
+
+            Assert.IsNotNull(properties);
+            Assert.AreEqual(2, properties.Count);
+
+            PropertyDescriptorBuilder builder2 = new PropertyDescriptorBuilder(new TestObject[] {new TestObject() { ivalue=100, str="str0"}, new TestObject() { ivalue=20, str="str1"} });
+            var properties2 = builder2.GetProperties();
+
+            Assert.IsNotNull(properties2);
+            Assert.AreEqual(2, properties2.Count);
+        }
+    }
+
+    public class TestObject
+    {
+        public string str { get; set; }
+        public int ivalue { get; set; }
+    }
+
+
+
+}
