3f5dcb1c5cc2b4c5176a9595d8aa3bfa6667c231
bodong <bodong@tencent.com>
2023-04-20 17:56:28 +0800

    add binding list edit.

---
+++ Avalonia.PropertyGrid/Controls/BindingListEdit.axaml.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using System.ComponentModel;
+
+namespace Avalonia.PropertyGrid.Controls
+{
+    public class BindingListEdit : TemplatedControl
+    {
+        public static readonly DirectProperty<BindingListEdit, IBindingList> DataListProperty =
+            AvaloniaProperty.RegisterDirect<BindingListEdit, IBindingList>(
+                nameof(DataList),
+                o => o.DataList,
+                (o, v) => o.DataList = v
+                );
+
+        IBindingList _DataList;
+        public IBindingList DataList
+        {
+            get => _DataList;
+            set => SetAndRaise(DataListProperty, ref _DataList, value);
+        }
+
+
+    }
+}
