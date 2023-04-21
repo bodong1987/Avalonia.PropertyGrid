56ceb0105d85d4a75b90663566286ad582bc64fa
bodong <bodong@tencent.com>
2023-04-21 16:04:35 +0800

    support custom type descriptions.

---
+++ Avalonia.PropertyGrid.Samples/Models/SimpleObject.cs
@@ -0,0 +1,140 @@
+﻿using Avalonia.PropertyGrid.Model.Collections;
+using Avalonia.PropertyGrid.Model.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.PropertyGrid.Model.ComponentModel;
+using Avalonia.PropertyGrid.Model.Extensions;
+
+namespace Avalonia.PropertyGrid.Samples.Models
+{
+    public class SimpleObject : ReactiveObject
+    {
+        public readonly string Description;
+
+        public SimpleObject(string description)
+        {
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"({GetHashCode()}){Description}";
+        }
+
+        [Category("String")]
+        [DisplayName("Target Name")]
+        public string Name { get; set; }
+
+        [Category("String")]
+        [DisplayName("Target Path")]
+        [PathBrowsable(Filters = "Image Files(*.jpg;*.png;*.bmp;*.tag)|*.jpg;*.png;*.bmp;*.tag")]
+        public string Path { get; set; }
+
+        [Category("String")]
+        public string UUID { get; set; } = Guid.NewGuid().ToString();
+
+        [Category("String")]
+        [PasswordPropertyText(true)]
+        public string Password { get; set; }
+
+        [Category("Boolean")]
+        public bool EncryptData { get; set; } = true;
+
+        [Category("Boolean")]
+        public bool SafeMode { get; set; } = false;
+
+        [Category("Boolean")]
+        public bool? ThreeStates { get; set; } = null;
+
+        [Category("Enum")]
+        public PhoneService Service { get; set; } = PhoneService.None;
+
+        [Category("Enum")]
+        public PlatformID CurrentPlatform => Environment.OSVersion.Platform;
+
+        [Category("Enum")]
+        public PlatformID Platform { get; set; } = Environment.OSVersion.Platform;
+
+        [Category("Selectable List")]
+        public SelectableList<string> LoginName { get; set; } = new SelectableList<string>(new string[] { "John", "David", "bodong" }, "bodong");
+
+        string _SourceImagePath;
+        [Category("DataValidation")]
+        [PathBrowsable(Filters = "Image Files(*.jpg;*.png;*.bmp;*.tag)|*.jpg;*.png;*.bmp;*.tag")]
+        public string SourceImagePath
+        {
+            get => _SourceImagePath;
+            set
+            {
+                if (value.IsNullOrEmpty())
+                {
+                    throw new ArgumentNullException(nameof(SourceImagePath));
+                }
+
+                if (!System.IO.Path.GetExtension(value).iEquals(".png"))
+                {
+                    throw new ArgumentException($"{nameof(SourceImagePath)} must be .png file.");
+                }
+
+                _SourceImagePath = value;
+            }
+        }
+
+        [Category("DataValidation")]
+        [Description("Select platforms")]
+        [ValidatePlatform]
+        public CheckedList<PlatformID> Platforms { get; set; } = new CheckedList<PlatformID>(Enum.GetValues(typeof(PlatformID)).Cast<PlatformID>());
+
+        [Category("Numeric")]
+        [Range(10, 200)]
+        public int iValue { get; set; } = 100;
+
+        [Category("Numeric")]
+        [Range(0.1f, 10.0f)]
+        public float fValue { get; set; } = 0.5f;
+
+        [Category("Numeric")]
+        public Int64 i64Value { get; set; } = 1000000000;
+
+        [Category("Binding List")]
+        public BindingList<string> stringList { get; set; } = new BindingList<string>() { "bodong", "china" };
+
+        [Category("Binding List")]
+        public BindingList<Boolean> boolList { get; set; } = new BindingList<bool> { true, false };
+
+        [Category("Binding List")]
+        public BindingList<PlatformID> enumList { get; set; } = new BindingList<PlatformID>() { PlatformID.Win32NT, PlatformID.Unix };
+    }
+
+    [Flags]
+    public enum PhoneService
+    {
+        None = 0,
+        LandLine = 1,
+        Cell = 2,
+        Fax = 4,
+        Internet = 8,
+        Other = 16
+    }
+
+    public class ValidatePlatformAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is CheckedList<PlatformID> id)
+            {
+                if (id.Contains(PlatformID.Unix) || id.Contains(PlatformID.Other))
+                {
+                    return new ValidationResult("Can't select Unix or Other");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- Avalonia.PropertyGrid.Samples/ViewModels/MainWindowViewModel.cs
+++ Avalonia.PropertyGrid.Samples/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,7 @@
-﻿using Avalonia.PropertyGrid.Model.Collections;
-using Avalonia.PropertyGrid.Model.ComponentModel;
-using Avalonia.PropertyGrid.Model.ComponentModel.DataAnnotations;
-using Avalonia.PropertyGrid.Model.Extensions;
+﻿using Avalonia.PropertyGrid.Samples.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
 namespace Avalonia.PropertyGrid.Samples.ViewModels
 {
@@ -28,132 +23,71 @@ namespace Avalonia.PropertyGrid.Samples.ViewModels
         public SimpleObject multiObject1 => _MultiObj1;
 
         public IEnumerable<SimpleObject> multiObjects => new SimpleObject[] { multiObject0, multiObject1 };
-    }
 
-    public class SimpleObject : ReactiveObject
-    {
-        public readonly string Description;
+        readonly ScriptableOptions _Options = new ScriptableOptions();
+        public ScriptableOptions customOptions => _Options;
 
-        public SimpleObject(string description)
+        public MainWindowViewModel()
         {
-            Description = description;
-        }
-
-        public override string ToString()
-        {
-            return $"({GetHashCode()}){Description}";
-        }
-
-        [Category("String")]
-        [DisplayName("Target Name")]
-        public string Name { get; set; }
-
-        [Category("String")]
-        [DisplayName("Target Path")]
-        [PathBrowsable(Filters = "Image Files(*.jpg;*.png;*.bmp;*.tag)|*.jpg;*.png;*.bmp;*.tag")]
-        public string Path { get; set; }
-
-        [Category("String")]
-        public string UUID { get; set; } = Guid.NewGuid().ToString();
-
-        [Category("String")]
-        [PasswordPropertyText(true)]
-        public string Password { get; set; }
-
-        [Category("Boolean")]
-        public bool EncryptData { get; set; } = true;
-
-        [Category("Boolean")]
-        public bool SafeMode { get; set; } = false;
-
-        [Category("Boolean")]
-        public bool? ThreeStates { get; set; } = null;
-
-        [Category("Enum")]
-        public PhoneService Service { get; set; } = PhoneService.None;
-
-        [Category("Enum")]
-        public PlatformID CurrentPlatform => Environment.OSVersion.Platform;
-
-        [Category("Enum")]
-        public PlatformID Platform { get; set; } = Environment.OSVersion.Platform;
-
-        [Category("Selectable List")]
-        public SelectableList<string> LoginName { get; set; } = new SelectableList<string>(new string[] { "John", "David", "bodong" }, "bodong");
-
-        string _SourceImagePath;
-        [Category("DataValidation")]
-        [PathBrowsable(Filters = "Image Files(*.jpg;*.png;*.bmp;*.tag)|*.jpg;*.png;*.bmp;*.tag")]
-        public string SourceImagePath
-        {
-            get => _SourceImagePath;
-            set
+            _Options.AddProperty(new ScriptableObject()
             {
-                if(value.IsNullOrEmpty())
-                {
-                    throw new ArgumentNullException(nameof(SourceImagePath));
-                }
+                Name = "IntValue",
+                DisplayName = "Int Value",
+                Description = "Custom type = int",
+                Value = 1024
+            });
 
-                if(!System.IO.Path.GetExtension(value).iEquals(".png"))
-                {
-                    throw new ArgumentException($"{nameof(SourceImagePath)} must be .png file.");
-                }
-
-                _SourceImagePath = value;
-            }
-        }
-
-        [Category("DataValidation")]
-        [Description("Select platforms")]
-        [ValidatePlatform]
-        public CheckedList<PlatformID> Platforms { get; set; } = new CheckedList<PlatformID>(Enum.GetValues(typeof(PlatformID)).Cast<PlatformID>());
-
-        [Category("Numeric")]
-        [Range(10, 200)]
-        public int iValue { get; set; } = 100;
-
-        [Category("Numeric")]
-        [Range(0.1f, 10.0f)]
-        public float fValue { get; set; } = 0.5f;
-
-        [Category("Numeric")]
-        public Int64 i64Value { get; set; } = 1000000000;
-
-        [Category("Binding List")]
-        public BindingList<string> stringList { get; set; } = new BindingList<string>() { "bodong", "china" };
-
-        [Category("Binding List")]
-        public BindingList<Boolean> boolList { get; set; } = new BindingList<bool> { true, false };
-
-        [Category("Binding List")]
-        public BindingList<PlatformID> enumList { get; set; } = new BindingList<PlatformID>() { PlatformID.Win32NT, PlatformID.Unix };
-    }
-
-    [Flags]
-    public enum PhoneService
-    {
-        None = 0,
-        LandLine = 1,
-        Cell = 2,
-        Fax = 4,
-        Internet = 8,
-        Other = 16
-    }
-
-    public class ValidatePlatformAttribute : ValidationAttribute
-    {
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if(value is CheckedList<PlatformID> id)
+            _Options.AddProperty(new ScriptableObject()
             {
-                if(id.Contains(PlatformID.Unix) || id.Contains(PlatformID.Other))
-                {
-                    return new ValidationResult("Can't select Unix or Other");
-                }
-            }
+                Name = "BooleanValue",
+                DisplayName = "Boolean Value",
+                Description = "Custom type = boolean",
+                Value = true
+            });
 
-            return ValidationResult.Success;
+            _Options.AddProperty(new ScriptableObject()
+            {
+                Name = "ThreeBooleanValue",
+                DisplayName = "Three Boolean Value",
+                Description = "Custom type = boolean?",
+                Value = null,
+                ValueType = typeof(bool?)
+            });
+
+            _Options.AddProperty(new ScriptableObject()
+            {
+                Name = "StringValue",
+                DisplayName = "String Value",
+                Description = "Custom type = string",
+                Value = "bodong"
+            });
+
+            _Options.AddProperty(new ScriptableObject()
+            {
+                Name = "EnumValue",
+                DisplayName = "Enum Value",
+                Description = "Custom type = Enum",
+                Value = Environment.OSVersion.Platform,
+                ExtraAttributes = new Attribute[] { new ValidatePlatformAttribute() }
+            });
+
+            _Options.AddProperty(new ScriptableObject()
+            {
+                Name = "EnumValueR",
+                DisplayName = "Enum Value(Readonly)",
+                Description = "Custom type = Enum",
+                Value = Environment.OSVersion.Platform,
+                ExtraAttributes = new Attribute[] { new ReadOnlyAttribute(true) } 
+            });
+
+            _Options.AddProperty(new ScriptableObject()
+            {
+                Name = "BindingList",
+                DisplayName = "BindingList Value",
+                Description = "Custom type = BindingList",
+                Value = new BindingList<int>() { 1024, 2048, 4096},
+                ExtraAttributes = new Attribute[] {new CategoryAttribute("BindingList")}
+            });
         }
     }
-
 }
