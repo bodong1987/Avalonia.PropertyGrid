a4fb4b3e1352682163d6beb5d483af4b82ce32f7
bodong1987 <dbdongbo@vip.qq.com>
2024-10-24 11:03:46 +0800

    remove old projects.

--- Avalonia.PropertyGrid.NugetSamples/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Avalonia.PropertyGrid.Samples.ViewModels;
-
-public class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- Avalonia.PropertyGrid.NugetSamples/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace Avalonia.PropertyGrid.Samples.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
