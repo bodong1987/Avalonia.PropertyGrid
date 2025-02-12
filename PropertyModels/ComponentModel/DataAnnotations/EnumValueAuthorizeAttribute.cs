829fdded523ffa2f8a79f0c9f2f7d3ede2b86df8
bodong1987 <dbdongbo@vip.qq.com>
2025-02-12 15:40:27 +0800

    change IEnumValueAuthorizeAttribute

--- PropertyModels/ComponentModel/DataAnnotations/EnumValueAuthorizeAttribute.cs
+++ PropertyModels/ComponentModel/DataAnnotations/EnumValueAuthorizeAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace PropertyModels.ComponentModel.DataAnnotations
 {
@@ -11,9 +12,11 @@ namespace PropertyModels.ComponentModel.DataAnnotations
         /// <summary>
         /// Determines whether the specified value is allowed.
         /// </summary>
-        /// <param name="value">The value to check.</param>
+        /// <param name="enumType">Type of the enum.</param>
+        /// <param name="fieldName">Name of the filed.</param>
+        /// <param name="fieldValue">The field value.</param>
         /// <returns><c>true</c> if the value is allowed; otherwise, <c>false</c>.</returns>
-        bool AllowValue(object value);
+        bool AllowValue(Type enumType, string fieldName, object fieldValue);
     }
 
     /// <summary>
@@ -26,16 +29,17 @@ namespace PropertyModels.ComponentModel.DataAnnotations
         /// <summary>
         /// Determines whether the specified enum value is allowed.
         /// </summary>
+        /// <param name="fieldName">Name of the filed.</param>
         /// <param name="value">The enum value to check.</param>
         /// <returns><c>true</c> if the value is allowed; otherwise, <c>false</c>.</returns>
-        bool AllowValue(T value);
+        bool AllowValue(string fieldName, T value);
     }
 
     /// <summary>
     /// Attribute to specify permitted enum values.
     /// </summary>
     /// <typeparam name="T">The enum type.</typeparam>
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
     public class EnumPermitValuesAttribute<T> : Attribute, IEnumValueAuthorizeAttribute<T>
         where T : Enum
     {
@@ -56,24 +60,66 @@ namespace PropertyModels.ComponentModel.DataAnnotations
         /// <summary>
         /// Determines whether the specified enum value is allowed.
         /// </summary>
+        /// <param name="fieldName">Name of the filed.</param>
         /// <param name="value">The enum value to check.</param>
         /// <returns><c>true</c> if the value is allowed; otherwise, <c>false</c>.</returns>
-        public virtual bool AllowValue(T value)
+        public virtual bool AllowValue(string fieldName, T value)
         {
             return PermitValues.Contains(value);
         }
 
-        bool IEnumValueAuthorizeAttribute.AllowValue(object value)
+        bool IEnumValueAuthorizeAttribute.AllowValue(Type enumType, string fieldName, object fieldValue)
         {
-            return value is T enumValue && AllowValue(enumValue);
+            Debug.Assert(enumType == typeof(T));
+
+            return fieldValue is T enumValue && AllowValue(fieldName, enumValue);
         }
     }
+
+    /// <summary>
+    /// Class EnumPermitNamesAttribute.
+    /// Implements the <see cref="Attribute" />
+    /// Implements the <see cref="PropertyModels.ComponentModel.DataAnnotations.IEnumValueAuthorizeAttribute" />
+    /// </summary>
+    /// <seealso cref="Attribute" />
+    /// <seealso cref="PropertyModels.ComponentModel.DataAnnotations.IEnumValueAuthorizeAttribute" />
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+    public class EnumPermitNamesAttribute : Attribute, IEnumValueAuthorizeAttribute
+    {
+        /// <summary>
+        /// The permit names
+        /// </summary>
+        public readonly HashSet<string> PermitNames;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumPermitNamesAttribute"/> class.
+        /// </summary>
+        /// <param name="permitNames">The permit names.</param>
+        public EnumPermitNamesAttribute(params string[] permitNames)
+        {
+            PermitNames = new HashSet<string>(permitNames);
+        }
+
+        /// <summary>
+        /// Determines whether the specified value is allowed.
+        /// </summary>
+        /// <param name="enumType">Type of the enum.</param>
+        /// <param name="fieldName">Name of the filed.</param>
+        /// <param name="fieldValue">The field value.</param>
+        /// <returns><c>true</c> if the value is allowed; otherwise, <c>false</c>.</returns>
+        public virtual bool AllowValue(Type enumType, string fieldName, object fieldValue)
+        {
+            return PermitNames.Contains(fieldName);
+        }
+    }
+
+
 
     /// <summary>
     /// Attribute to specify prohibited enum values.
     /// </summary>
     /// <typeparam name="T">The enum type.</typeparam>
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
     public class EnumProhibitValuesAttribute<T> : Attribute, IEnumValueAuthorizeAttribute<T>
         where T : Enum
     {
@@ -94,16 +140,56 @@ namespace PropertyModels.ComponentModel.DataAnnotations
         /// <summary>
         /// Determines whether the specified enum value is allowed.
         /// </summary>
+        /// <param name="fieldName">Name of the filed.</param>
         /// <param name="value">The enum value to check.</param>
         /// <returns><c>true</c> if the value is allowed; otherwise, <c>false</c>.</returns>
-        public virtual bool AllowValue(T value)
+        public virtual bool AllowValue(string fieldName, T value)
         {
             return !ProhibitValues.Contains(value);
         }
 
-        bool IEnumValueAuthorizeAttribute.AllowValue(object value)
+        bool IEnumValueAuthorizeAttribute.AllowValue(Type enumType, string fieldName, object fieldValue)
         {
-            return value is T enumValue && AllowValue(enumValue);
+            Debug.Assert(enumType == typeof(T));
+
+            return fieldValue is T enumValue && AllowValue(fieldName, enumValue);
+        }
+    }
+
+    /// <summary>
+    /// Class EnumProhibitNamesAttribute.
+    /// Implements the <see cref="Attribute" />
+    /// Implements the <see cref="PropertyModels.ComponentModel.DataAnnotations.IEnumValueAuthorizeAttribute" />
+    /// </summary>
+    /// <seealso cref="Attribute" />
+    /// <seealso cref="PropertyModels.ComponentModel.DataAnnotations.IEnumValueAuthorizeAttribute" />
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+    public class EnumProhibitNamesAttribute : Attribute, IEnumValueAuthorizeAttribute
+    {
+        /// <summary>
+        /// The prohibit names
+        /// </summary>
+        public readonly HashSet<string> ProhibitNames;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumProhibitNamesAttribute"/> class.
+        /// </summary>
+        /// <param name="prohibitNames">The prohibit names.</param>
+        public EnumProhibitNamesAttribute(params string[] prohibitNames)
+        {
+            ProhibitNames = new HashSet<string>(prohibitNames);
+        }
+
+        /// <summary>
+        /// Determines whether the specified value is allowed.
+        /// </summary>
+        /// <param name="enumType">Type of the enum.</param>
+        /// <param name="fieldName">Name of the filed.</param>
+        /// <param name="fieldValue">The field value.</param>
+        /// <returns><c>true</c> if the value is allowed; otherwise, <c>false</c>.</returns>
+        public virtual bool AllowValue(Type enumType, string fieldName, object fieldValue)
+        {
+            return !ProhibitNames.Contains(fieldName);
         }
     }
 }
