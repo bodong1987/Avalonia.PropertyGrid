827cca3cbea9747cfc45118480982085011586fc
bodong1987 <dbdongbo@vip.qq.com>
2025-02-25 10:54:50 +0800

    add single selection mode attribute.

---
+++ Sources/PropertyModels/ComponentModel/SingleSelectionModeAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PropertyModels.ComponentModel;
+
+/// <summary>
+/// selection mode, used for normal enum type or ISelectableList
+/// </summary>
+public enum SingleSelectionMode
+{
+    /// <summary>
+    /// default
+    /// </summary>
+    Default,
+    /// <summary>
+    /// default/combobox
+    /// </summary>
+    ComboBox = Default,
+    /// <summary>
+    /// radio
+    /// </summary>
+    Radio,
+    /// <summary>
+    /// radio button
+    /// </summary>
+    RadioButton,
+}
+
+/// <summary>
+/// set select mode
+/// </summary>
+[AttributeUsage(AttributeTargets.Property|AttributeTargets.Field, AllowMultiple = false)]
+public class SingleSelectionModeAttribute : Attribute
+{
+    /// <summary>
+    /// Mode
+    /// </summary>
+    public readonly SingleSelectionMode Mode;
+
+    /// <summary>
+    /// construct this instance
+    /// </summary>
+    /// <param name="mode"></param>
+    public SingleSelectionModeAttribute(SingleSelectionMode mode)
+    {
+        Mode = mode;
+    }
+}
