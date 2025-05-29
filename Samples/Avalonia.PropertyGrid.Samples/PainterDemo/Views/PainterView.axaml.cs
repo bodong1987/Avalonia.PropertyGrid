79ee944cc58c46dc5ef606ab8b55adacbaec89d7
bodong1987 <dbdongbo@vip.qq.com>
2025-05-29 21:19:59 +0800

    tests

---
+++ Samples/Avalonia.PropertyGrid.Samples/PainterDemo/Views/PainterView.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Input;
+using Avalonia.Media;
+
+namespace Avalonia.PropertyGrid.Samples.PainterDemo.Views;
+
+public partial class PainterView : UserControl
+{
+    public PainterView()
+    {
+        InitializeComponent();
+    }
+    
+    private void OnShapePointerPressed(object sender, PointerPressedEventArgs e)
+    {
+        if (sender is Shape shape)
+        {
+        }
+    }
+}
