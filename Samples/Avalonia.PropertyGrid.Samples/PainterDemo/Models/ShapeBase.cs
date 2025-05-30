193c163824218668752e1601fc997b70f9e66052
bodong1987 <dbdongbo@vip.qq.com>
2025-05-30 14:39:36 +0800

    more options.

--- Samples/Avalonia.PropertyGrid.Samples/PainterDemo/Models/ShapeBase.cs
+++ Samples/Avalonia.PropertyGrid.Samples/PainterDemo/Models/ShapeBase.cs
@@ -1,14 +1,23 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
+using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Media;
 using PropertyModels.ComponentModel;
+using PropertyModels.ComponentModel.DataAnnotations;
+
 // ReSharper disable CompareOfFloatsByEqualityOperator
 // ReSharper disable PropertyCanBeMadeInitOnly.Global
 
 namespace Avalonia.PropertyGrid.Samples.PainterDemo.Models;
+
+public enum ShapeFillMode
+{
+    Fill,
+    Blank
+}
 
 public abstract class ShapeBase : MiniReactiveObject
 {
@@ -43,10 +52,29 @@ public abstract class ShapeBase : MiniReactiveObject
             }
         }
     }
+    
+    private ShapeFillMode _fillMode = ShapeFillMode.Fill;
+
+    [Category("Apperance")]
+    [ConditionTarget]
+    [SingleSelectionMode(SingleSelectionMode.ToggleButtonGroup)]
+    public ShapeFillMode FillMode
+    {
+        get => _fillMode;
+        set
+        {
+            if (_fillMode != value)
+            {
+                _fillMode = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
 
     private Color _fillColor = Colors.Gray;
     
     [Category("Appearance")]
+    [VisibilityPropertyCondition(nameof(FillMode), ShapeFillMode.Fill)]
     public Color FillColor
     {
         get => _fillColor;
@@ -109,8 +137,85 @@ public abstract class ShapeBase : MiniReactiveObject
         }
     }
     
-    protected readonly SolidColorBrush FillBrush = new SolidColorBrush(Colors.Black);
-    protected  readonly SolidColorBrush StrokeBrush = new SolidColorBrush(Colors.White);
+    private PenLineCap _strokeLineCap = PenLineCap.Flat;
+    [Category("Appearance")]
+    public PenLineCap StrokeLineCap
+    {
+        get => _strokeLineCap;
+        set
+        {
+            if (_strokeLineCap != value)
+            {
+                _strokeLineCap = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    private PenLineJoin _strokeJoin = PenLineJoin.Miter;
+    [Category("Appearance")]
+    public PenLineJoin StrokeJoin
+    {
+        get => _strokeJoin;
+        set
+        {
+            if (_strokeJoin != value)
+            {
+                _strokeJoin = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    private double _strokeDashOffset = 0.0;
+    [Category("Appearance")]
+    [FloatPrecision(0)]
+    public double StrokeDashOffset
+    {
+        get => _strokeDashOffset;
+        set
+        {
+            if (_strokeDashOffset != value)
+            {
+                _strokeDashOffset = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    private BindingList<double> _strokeDashArray = [];
+    
+    [Category("Appearance")]
+    public BindingList<double> StrokeDashArray
+    {
+        get => _strokeDashArray;
+        set
+        {
+            if (_strokeDashArray != value)
+            {
+                _strokeDashArray = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    private Stretch _stretch = Stretch.None;
+    [Category("Appearance")]
+    public Stretch Stretch
+    {
+        get => _stretch;
+        set
+        {
+            if (_stretch != value)
+            {
+                _stretch = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+    
+    protected readonly SolidColorBrush FillBrush = new (Colors.Black);
+    protected  readonly SolidColorBrush StrokeBrush = new (Colors.White);
     
     public abstract Shape CreateAvaloniaShape();
 
@@ -119,10 +224,27 @@ public abstract class ShapeBase : MiniReactiveObject
         FillBrush.Color = FillColor;
         StrokeBrush.Color = StrokeColor;
         
-        shape.Fill = FillBrush;
+        shape.Fill = FillMode == ShapeFillMode.Fill ? FillBrush : null;
         shape.Stroke = StrokeBrush;
         shape.Opacity = Opacity;
         shape.StrokeThickness = StrokeThickness;
+        shape.StrokeLineCap = StrokeLineCap;
+        shape.StrokeJoin = StrokeJoin;
+        shape.StrokeDashOffset = StrokeDashOffset;
+        shape.StrokeDashArray?.Clear();
+        shape.Stretch = Stretch;
+        
+        if (StrokeDashArray.Count > 0)
+        {
+            if (shape.StrokeDashArray == null)
+            {
+                shape.StrokeDashArray = new AvaloniaList<double>(StrokeDashArray);
+            }
+            else
+            {
+                shape.StrokeDashArray.AddRange(StrokeDashArray);    
+            }
+        }
         
         Canvas.SetLeft(shape, X);
         Canvas.SetTop(shape, Y);
